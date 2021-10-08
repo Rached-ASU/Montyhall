a7af9d9a8d728848c0bb40886e69e214df54d0b5
Rached-ASU <69304613+Rached-ASU@users.noreply.github.com>
2021-10-08 06:14:08 +0100

    intial comment

--- Montyhall/R/monty-hall-problem.R
+++ Montyhall/R/monty-hall-problem.R
@@ -34,19 +34,23 @@ create_game <- function()
 
 
 
-#' @title selecting one of the three door randomly
+#' @title
+#'   Player selects a door.
 #'
-#' @description `select_door()`generate a random door
+#' @description
+#'   `select_door()` generates a random door.
 #'
-#' @details this function will represent the choice of the participant where he
-#' will have equal chances to select a door
+#' @details
+#'   this function will represent the choice of the participant where he will have equal chances to select a door.
+#'   
 #'
 #' @param ... no arguments are used by the function.
 #'
-#' @return The function returns a length of 1 character vector
+#' @return  The function returns a length of 1 character vector
 #'   indicating the selected door
 #'
-#' @examples select_door()
+#' @examples
+#'  select_door()
 #'
 #' @export
 select_door <- function( )
@@ -58,25 +62,30 @@ select_door <- function( )
 
 
 
-#' @title Selecting one goat door
+#' @title
+#' Host opens one goat door
 #'
-#' @description `open_goat_door` generates a number referring to a door with one goat behind
-#' different than the one previously selected
+#' @description
+#'    `open_goat_door()` This function will generate a number referring to a door with one goat behind
+#'different from the one previously selected
 #'
 #' @details
 #' The host will make the game with more suspense will choose a door with one goat behind it and
-#' will leave the participant with two doors that one contains another
-#' goat and the other contain the car
+#' will leave the participant with two doors that one contains another goat and the other contain the car
+#'  
+#'  
+#'  
+#'  
 #'
-#' @param tis function will take two parameters, the first parameter game is a vector indicating the positions
-#' of goats and the car.
+#' @param ... this function will take two parameters the first parameter game is a vector indicating the positions of goats and the car.
+#'  ( game, a.pick )
 #'
-#'  the second parameter will define the door previously selected from the participant
+#' @return The function returns a  numeric vector of length one
+#'   the second parameter will define the door previously selected from the participant indicating the opened door by host.
 #'
-#' @return the function returns a length of 1 character vector
-#'   indicating the opened door
+#' @examples
+#' open_goat_door(game, a.pick)
 #'
-#' @examples open_goat_door ( game, a.pick)
 #'
 #' @export
 open_goat_door <- function( game, a.pick )
@@ -99,25 +108,27 @@ open_goat_door <- function( game, a.pick )
 
 
 #' @title
-#' Give a chance to change the door
+#' Chance to change doors
 #'
 #' @description
-#' `change_door` Asks the user to change or keep the previously selected door
+#'   Asks the user to change or keep the previously selected door
+#'  
 #'
 #' @details
 #'  The game will give a last chance to the user to either keep his door or change it and with
 #'  this game we will try to get the results of the two scenarios
+#'  
+#'  
+#' 
+#'  
 #'
-#' @param Stay a Boolean that can take wither true if the user will change the door or false when
-#' the user will keep the original door
+#' @param ... a Boolean that can take wither true if the user will change the door or false when the user will keep the original door
 #'
-#' opened.door a vector that will indicate which door that was previously opened
+#' @return The function returns a length 1 numeric vector indicating the final pick of the participant
 #'
-#' a pick a vector that will indicate the door that was first selected
+#' @examples
+#' change_door( stay=T, opened.door, a.pick )
 #'
-#' @return the function returns a length 1 vector indicating the final pick of the participant
-#'
-#' @examples function( stay=T, opened.door, a.pick )
 #'
 #' @export
 change_door <- function( stay=T, opened.door, a.pick )
@@ -138,18 +149,27 @@ change_door <- function( stay=T, opened.door, a.pick )
 
 
 
-#' @title Determine if the participant won
+#' @title
+#' Determine if the participant won
 #'
-#' @description `determine_winner` generates a Win or lose depending on the situation
+#' @description
 #'
-#' @details This function will show the user if he won or lost depending on his final pick.
+#'  The function determines if contestant has won or lost depending on the situation of the final selection
+#'   .
+#'
+#' @details
+#' This function will show the user if he won or lost depending on his final pick.
 #' If door behind the final pick was a car that the player win and if not the player will lose
+#' 
 #'
-#' @param  final pick a vector that will indicate the door that was last selected
+#' @param ... final pick a vector that will indicate the door that was last selected (final.pick,game)
 #'
-#' @return The function will return a string either WIN or Lose
+#' @return The function returns a WIN or LOSE
 #'
-#' @examples determine_winner( final.pick, game )
+#' @examples
+#' determine_winner( final.pick, game )
+#'
+#'
 #' @export
 determine_winner <- function( final.pick, game )
 {
@@ -167,22 +187,28 @@ determine_winner <- function( final.pick, game )
 
 
 
-#' @title Play the game one time
+#' @title
+#' Play the game one time
 #'
-#' @description play_game general function that contains all the previous functions will return
-#' WIN  or Lose
+#' @description
+#' Play_game is a general function that contains all the previous functions
+#' of a single game in order and see which strategy wins/loses.
 #'
-#' @details The game consist of choosing one door from three doors, one of which has a car
+#' @details
+#'  The game consist of choosing one door from three doors, one of which has a car
 #' behind it and two have goats The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. If the last pick was a car the participants
-#'   considered a winner otherwise a looser
-#' @param N/A
+#' opens a door to reveal a goat, and then the contestant is
+#' given an opportunity to stay with their original selection
+#' or switch to the other unopened door. If the last pick was a car the participants
+#' considered a winner otherwise a looser
+#' 
+#' @param ... no arguments are used by the function.
 #'
-#' @return will return a small table that indicates the winngin and losing strategy
+#' @return
+#' will return a small table that indicates the winning and losing strategy
 #'
-#' @examples play_game()
+#' @examples
+#' play_game()
 #'
 #' @export
 play_game <- function( )
@@ -204,26 +230,30 @@ play_game <- function( )
   return( game.results )
 }
 
-play_game()
 
 
 
-#' @title Play the game one hundred time
-#' @description play_n_game is a general function that contains all the previous functions will repeat the game 100 times
-#'  and will return  WIN  or Lose
+
+
+#' @title
+#' Monty Hall game simulation
 #'
-#' @details The game consist of choosing one door from three doors, one of which has a car
-#' behind it and two have goats The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. If the last pick was a car the participants
-#'   considered a winner otherwise a looser
+#' @description
+#' Allows user to run the simulation for n times to know the WIN/LOSE probability.
 #'
-#' @param n which indicates how many times the game is repeated
+#' @details
+#' The function will show the game outcomes if contestant stays or switches
+#' and it takes into account the number of times the contestant plays.Results are saved in
+#' a data frame showing probabilities of the two game strategies ( stay and switch)
 #'
-#' @return  will return a small table that indicates the winning and losing strategy
+#' @param  ... n which indicates how many times the game is repeated
 #'
-#' @examples function( 100 )
+#' @return
+#'  number of times where stay and switch strategies show WIN and LOSE outcomes.
+#'
+#' @examples
+#' play_n_games()
+#'
 #'
 #' @export
 play_n_games <- function( n=100 )
@@ -250,4 +280,3 @@ play_n_games <- function( n=100 )
   return( results.df )
 
 }
-
